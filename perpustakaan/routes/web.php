56a79a028ef0647ccc3b57d3aa9cd8c54bd4667f
aisyahhputr <91835362+Aisyahputri31@users.noreply.github.com>
2021-11-30 21:36:42 +0800

    pembaruan

--- perpustakaan/routes/web.php
+++ perpustakaan/routes/web.php
@@ -19,12 +19,12 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/', 'HomeController@index');
-/*
+
 Route::get('/user', 'UserController@index');
 Route::get('/user-register', 'UserController@create');
 Route::post('/user-register', 'UserController@store');
 Route::get('/user-edit/{id}', 'UserController@edit');
-*/
+
 Route::resource('user', 'UserController');
 
 Route::resource('anggota', 'AnggotaController');
